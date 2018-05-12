429e079ad67a9eb88ea05bd0bc94ee7ba156dedc
glenn vincent hinks <ghinks@yahoo.com>
2018-05-12 12:09:29 -0400

    branch testing

--- src/authentication/index.js
+++ src/authentication/index.js
@@ -1,17 +1,15 @@
 import parse from 'parse-git-config'
 import os from 'os'
+import get from 'lodash.get'
 
-const getEnvToken = () => {
-  return process.env.GITHUB_TOKEN
-}
+const getEnvToken = () => process.env.GITHUB_TOKEN
 
 const getConfigToken = () => {
   console.log(parse)
   const config = parse.sync({
     path: `${os.homedir()}/.gitconfig`
   })
-  if (config['github-search'] &&
-    config['github-search'].token) {
+  if (get(config, 'github-search.token')) {
     return config['github-search'].token
   }
   return undefined
--- src/authentication/test.js
+++ src/authentication/test.js
@@ -1,36 +1,68 @@
 import { getEnvToken, getConfigToken, __RewireAPI__ as authRewire } from './index'
 
 describe('authentication', () => {
-  let envToken
-  const gitconfig = {
-    'user': {
-      'name': 'glenn vincent hinks',
-      'email': 'ghinks@yahoo.com'
-    },
-    'core': {
-      'excludesfile': '/Users/developer/.gitignore_global',
-      'autocrlf': 'input'
-    },
+  describe('Passing', () => {
+    const gitconfig = {
+      'user': {
+        'name': 'glenn vincent hinks',
+        'email': 'ghinks@yahoo.com'
+      },
+      'core': {
+        'excludesfile': '/Users/developer/.gitignore_global',
+        'autocrlf': 'input'
+      },
 
-    'github-search': {
-      'token': 'abc'
+      'github-search': {
+        'token': 'abc'
+      }
     }
-  }
+    let envToken
+    beforeAll(() => {
+      envToken = process.env.GITHUB_TOKEN
+      process.env.GITHUB_TOKEN = 'abc'
+      authRewire.__Rewire__('parse', { sync: () => gitconfig })
+      jest.resetModules()
+    })
+    afterAll(() => {
+      process.env.GITHUB_TOKEN = envToken
+      authRewire.__ResetDependency__('parse')
+    })
+    test('get github token', () => {
+      expect(getEnvToken()).toMatchSnapshot()
+    })
 
-  beforeAll(() => {
-    envToken = process.env.GITHUB_TOKEN
-    process.env.GITHUB_TOKEN = 'abc'
-    authRewire.__Rewire__('parse', { sync: () => gitconfig })
-  })
-  afterAll(() => {
-    process.env.GITHUB_TOKEN = envToken
-    authRewire.__ResetDependency__('parse')
-  })
-  test('get github token', () => {
-    expect(getEnvToken()).toMatchSnapshot()
+    test('get config token', () => {
+      expect(getConfigToken()).toMatchSnapshot()
+    })
   })
 
-  test('get config token', () => {
-    expect(getConfigToken()).toMatchSnapshot()
+  describe('failing', () => {
+    const gitconfig = {
+      'user': {
+        'name': 'glenn vincent hinks',
+        'email': 'ghinks@yahoo.com'
+      },
+      'core': {
+        'excludesfile': '/Users/developer/.gitignore_global',
+        'autocrlf': 'input'
+      }
+    }
+    let envToken
+    beforeAll(() => {
+      envToken = process.env.GITHUB_TOKEN
+      process.env.GITHUB_TOKEN = undefined
+      authRewire.__Rewire__('parse', { sync: () => gitconfig })
+      jest.resetModules()
+    })
+    afterAll(() => {
+      process.env.GITHUB_TOKEN = envToken
+      authRewire.__ResetDependency__('parse')
+    })
+    test('get github token', () => {
+      expect(getEnvToken()).toBe('undefined')
+    })
+    test('get no config token', () => {
+      expect(getConfigToken()).toBeUndefined()
+    })
   })
 })
