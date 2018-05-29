2b90e930fb12df6af4a5cbe43e1a898b9f61b89b
glenn vincent hinks <ghinks@yahoo.com>
2018-05-29 17:40:11 -0400

    test updates set issue age to 7 days.

--- src/format/test.js
+++ src/format/test.js
@@ -1,12 +1,17 @@
 import format from './index'
 import fsp from 'fs/promises'
+import moment from 'moment'
 
 describe('format', () => {
   let nodes
+
+  const setPublishedAt = () => new Date(moment().subtract(7, 'days'))
+
   beforeAll((done) => {
     fsp.readFile(`${__dirname}/test.data.json`, 'utf8')
       .then((testData) => {
         nodes = JSON.parse(testData)['nodes']
+        nodes[0].publishedAt = setPublishedAt()
         done()
       })
   })
